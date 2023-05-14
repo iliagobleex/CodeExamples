72afa698339f662f0cec63ff8c91dc8b190a51ba
iliagobleex <133578246+iliagobleex@users.noreply.github.com>
2023-05-14 23:38:58 +0100

    Add files via upload

---
+++ DogAge.js
@@ -0,0 +1,16 @@
+//setting myAge variable to my age
+var myAge = 18
+//setting earlyYears variable value to 2
+let earlyYears = 2
+
+earlyYears = earlyYears * 10.5;
+//subtracting 2 from myAge and reassigning to a  variable called laterYears
+let laterYears = myAge - 2
+//multiplying and assigning the value of laterYears to times 4
+laterYears *= 4;
+//adding two variables to find my age in dog years
+myAgeInDogYears = earlyYears + laterYears;
+//assigning lowercase letters to myName string
+var myName = 'Ilia Goble'.toLowerCase();
+//printing the results of the code
+console.log(`My name is ${myName}. I am ${myAge} which is ${myAgeInDogYears} years old in dog years.`);
